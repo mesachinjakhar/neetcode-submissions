df71fd8dd69ba90a12efdcce9754900e14c3b154
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-04-26 23:55:24 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet; 
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let n = s.len(); 
+        if n == 0 || n == 1 {
+            return n as i32; 
+        }
+
+        let mut set = HashSet::new();
+        let mut left = 0; 
+        let mut right = 0; 
+        let s_bytes = s.as_bytes();
+
+        let mut ans = 0; 
+
+        while right < n {
+            if set.contains(&s_bytes[right]) {
+                while set.contains(&s_bytes[right]) {
+                   set.remove(&s_bytes[left]);
+                    left += 1; 
+                } 
+            } else {
+                set.insert(s_bytes[right]);
+                let dist = right - left + 1; 
+                ans = ans.max(dist);
+                right += 1; 
+            }
+        }
+
+        return ans as i32; 
+
+    }
+}
